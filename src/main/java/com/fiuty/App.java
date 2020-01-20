c005b129695ea48d8fe63d2d3053cd4717339400
郑达悦 <zhengdayue@idaoben.com>
2020-01-20 14:41:21 +0800

    纯java maven项目,添加slf4j+log4j的依赖,加上log4j.properties文件的配置

---
+++ src/main/java/com/fiuty/App.java
@@ -0,0 +1,14 @@
+package com.fiuty;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        log.debug("hello {}", "debug");
+    }
+}
